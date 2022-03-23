bf25c29409b7a17a5abe97d83010705408e12454
Renato de Oliveira Lucena <cpdrenato@gmail.com>
2022-03-22 23:57:28 -0300

    criando a estrutura

---
+++ database/migrations/2022_03_22_224026_create_historics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoricsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historics', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku');
+            // $table->integer('quantity');
+            $table->float('quantity', 10, 3)->nullable();
+            $table->json('qtd')->nullable();
+            // FK
+            $table->foreignId('productID')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historics');
+    }
+}
